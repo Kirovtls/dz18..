b755af56185e1a497067db8e3017965862fa6fdc
rakhat nazarkulova <rakhatnazarkulova@rakhats-MacBook-Air.local>
2022-02-28 05:16:08 -0600

    Initial commit

---
+++ src/com/company/Cow.java
@@ -0,0 +1,7 @@
+package com.company;
+
+    public class Cow extends HomeJanibarlar {
+        public Cow(double weight, int age, char gender, String nickName) {
+            super(weight, age, gender, nickName);
+        }
+    }
